22068e4a43f1febc7bb45bd9b7f3a3ade0b05e7a
Sebiover <sebastianoverzier@web.de>
2023-05-30 18:20:30 +0200

    erster Code

---
+++ tester.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="c:\webdrivers\geckodriver.exe")
+
+driver.get("http://www.youtube.com")
